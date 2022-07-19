57eedb38b2f22aa612bce4c7e1af9f382cfb05c1
mehmet <tas349873@gmail.com>
2022-07-19 20:46:00 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import AppRouter from './router/AppRouter';
 
 const App = () => {
   return (
     <div>
-      <p>App</p>
+      <AppRouter/>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
-
+import React from "react";
+import {Link} from "react-router-dom";
 const Navbar = () => {
+  const currentUser = {displayName:"feliz franko"}
+  // const currentUser = false
   return (
     <div>
-      <p>Navbar</p>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Link to ={"/"} className = "navbar-brand text-white">
+            <h4>React Movie App</h4>
+          </Link>
+          <div className="d-flex text-white align-items-center">
+            {currentUser ? (
+              <>
+              <h5 className="mb-0 text-capitalize">{currentUser.displayName}</h5>
+              <button className="ms-2 btn btn-outline-light">Logout</button>
+              </>
+            ): (
+              <>
+              <button className="ms-2 btn btn-outline-light">Login</button>
+              <button className="ms-2 btn btn-outline-light">Register</button>
+              </>
+            )}
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
