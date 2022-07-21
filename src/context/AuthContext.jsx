72deeaf1063cced3b7eb86dfa6a28d32a5c3d6b0
mehmet <tas349873@gmail.com>
2022-07-22 00:23:17 +0300

    update

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,16 +1,23 @@
-import React, { useContext } from "react";
-import { useState } from "react";
-import { createContext } from "react";
-
-
+import React, { useEffect } from 'react';
+// import { useContext } from 'react';
+import { useState } from 'react';
+import { createContext } from 'react';
+import { userObserver } from '../auth/firebase';
 
 export const AuthContext = createContext();
-export const useAuthContext=()=>{
-    return useContext(AuthContext)
-}
+//* with custom hook
+// export const useAuthContext = () => {
+//   return useContext(AuthContext)
+// }
 
 const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(false);
+
+  useEffect(() => {
+    // setCurrentUser(JSON.parse(sessionStorage.getItem('user')));
+    userObserver(setCurrentUser);
+  }, []);
+
   return (
     <AuthContext.Provider value={{ currentUser }}>
       {children}
