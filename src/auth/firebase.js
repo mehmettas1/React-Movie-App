e7bcf85e13a54969185ac5be995f156dd39d9c74
mehmet <tas349873@gmail.com>
2022-07-22 07:16:34 +0300

    update

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -2,10 +2,19 @@ import { initializeApp } from 'firebase/app';
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  GoogleAuthProvider,
   onAuthStateChanged,
+  sendPasswordResetEmail,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
+  updateProfile,
 } from 'firebase/auth';
+import {
+  toastErrorNotify,
+  toastSuccessNotify,
+  toastWarnNotify,
+} from '../helpers/ToastNotify';
 
 // TODO: Replace the following with your app's Firebase project configuration
 //* https://firebase.google.com/docs/auth/web/start
@@ -26,7 +35,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export const createUser = async (email, password, navigate) => {
+export const createUser = async (email, password, navigate, displayName) => {
   //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
   try {
     let userCredential = await createUserWithEmailAndPassword(
@@ -34,10 +43,15 @@ export const createUser = async (email, password, navigate) => {
       email,
       password
     );
+    //? kullanıcı profilini güncellemek için kullanılan firebase metodu
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
+    toastSuccessNotify('Registered successfully!');
     navigate('/');
     console.log(userCredential);
   } catch (err) {
-    console.log(err);
+    toastErrorNotify(err.message);
   }
 };
 
@@ -53,9 +67,11 @@ export const signIn = async (email, password, navigate) => {
       password
     );
     navigate('/');
+    toastSuccessNotify('Logged in successfully!');
     // sessionStorage.setItem('user', JSON.stringify(userCredential.user));
     console.log(userCredential);
   } catch (err) {
+    toastErrorNotify(err.message);
     console.log(err);
   }
 };
@@ -74,4 +90,40 @@ export const userObserver = (setCurrentUser) => {
 
 export const logOut = () => {
   signOut(auth);
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Google
+//! Google ile girişi enable yap
+//* => Authentication => sign-in-method => Authorized domains => add domain
+//! Projeyi deploy ettikten sonra google sign-in çalışması için domain listesine deploy linkini ekle
+export const signUpProvider = (navigate) => {
+  //? Google ile giriş yapılması için kullanılan firebase metodu
+  const provider = new GoogleAuthProvider();
+  //? Açılır pencere ile giriş yapılması için kullanılan firebase metodu
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+      navigate('/');
+      toastSuccessNotify('Logged out successfully!');
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      console.log(error);
+    });
+};
+
+export const forgotPassword = (email) => {
+  //? Email yoluyla şifre sıfırlama için kullanılan firebase metodu
+  sendPasswordResetEmail(auth, email)
+    .then(() => {
+      // Password reset email sent!
+      toastWarnNotify('Please check your mail box!');
+      // alert("Please check your mail box!");
+    })
+    .catch((err) => {
+      toastErrorNotify(err.message);
+      // alert(err.message);
+      // ..
+    });
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,38 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { forgotPassword, signIn, signUpProvider } from '../auth/firebase.js';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
+    signIn(email, password, navigate);
+    // console.log(email, password);
   };
+  const handleProviderLogin = () => {
+    signUpProvider(navigate);
+  };
+
   return (
     <div className="d-flex justify-content-center">
-      <div className="form-image d-none d-md-block">
-        <img src="https://picsum.photos/800/800" alt="random-image" />
+      <div className="form-image d-none d-md-block ">
+        <img src={'https://picsum.photos/800/800'} alt="sample-movie" />
       </div>
       <div className="register-form">
-        <h1 className="form-title display-3">Login</h1>
-        <form id="register" onSubmit={handleSubmit}>
+        <h1 className="form-title display-3 ">Login</h1>
+        <form id="register" onSubmit={handleLogin}>
           <div className="mb-3">
             <label htmlFor="email" className="form-label">
               Email
             </label>
             <input
               type="email"
-              id="email"
               className="form-control"
-              placeholder="Enter your email name"
+              id="email"
+              placeholder="Enter your email adress.."
               required
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -34,15 +43,28 @@ const Login = () => {
             </label>
             <input
               type="password"
-              id="password"
               className="form-control"
-              placeholder="Enter your password name"
+              id="password"
+              placeholder="Enter your password.."
               required
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <input type="submit" className="btn btn-primary" value="Register" />
+          <div className="link" onClick={() => forgotPassword(email)}>
+            Forgot Password?
+          </div>
+          <input
+            type="submit"
+            className="btn btn-primary form-control"
+            value="Login"
+          />
         </form>
+        <button
+          className="btn btn-primary form-control"
+          onClick={handleProviderLogin}
+        >
+          Continue with Google
+        </button>
       </div>
     </div>
   );
